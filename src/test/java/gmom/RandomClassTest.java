6bd40331b1c90242fb89c5964f81ae378123a745
sergei.riabinin <sergei.riabinin@getbrains.com>
2022-05-13 15:08:49 +0200

    Initial commit

---
+++ src/test/java/gmom/RandomClassTest.java
@@ -0,0 +1,12 @@
+package gmom;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class RandomClassTest {
+
+    @Test
+    void nothingInterestingHere() {
+        Assertions.assertTrue(true);
+    }
+}
---
+++ src/test/java/module-info.java
@@ -0,0 +1,4 @@
+open module testModule {
+    requires srcModule;
+    requires org.junit.jupiter.api;
+}
